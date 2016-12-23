1773cba2c7153f7f753b2a982ee730d144021e05
Muhammad441 <muhammadsuhail441@gmail.com>
2016-12-23 19:31:14 +0530

    Update zeta.cpp

--- catkin_ws/src/robomuse/src/zeta.cpp
+++ catkin_ws/src/robomuse/src/zeta.cpp
@@ -1,10 +1,14 @@
 #include <ros/ros.h>
-    #include <tf/transform_broadcaster.h>
-    #include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#include"robomuse/chapter2_msg1.h"
+#include <nav_msgs/Odometry.h>
 #include"geometry_msgs/Point.h"
+#include<fstream>
+#include<iostream>
+
      double x = 0.0;
      double y = 0.0;
-     double th = 0.0,dth;
+     double th = 3.14/2,dth;
      double vx ;
      double vy ;
      double vth ;
@@ -12,29 +16,40 @@
      double Dr;
      double Distancel,Distancer,LastDistancel,LastDistancer;
 double L=0.56;
+/*
  void messageCallback(const geometry_msgs::Point::ConstPtr& msg)
 {
-    Distancel=msg->x;
-Distancer=msg->y;
+  Distancel=msg->x;
+  Distancer=msg->y;
+  Distancel=Distancel/8192*2*3.14*.06;
+  Distancer=Distancer/8192*2*3.14*.06;
+
+} */  
+ void messageCallback(const robomuse::chapter2_msg1& msg)
+{
+  Distancel=msg.A;
+  Distancer=msg.B;
   Distancel=Distancel/8192*2*3.14*.06;
   Distancer=Distancer/8192*2*3.14*.06;
 
 }   
+
     int main(int argc, char** argv){
       ros::init(argc, argv, "odometry_publisher");
       ros::NodeHandle nh;
-ros::Subscriber sub = nh.subscribe("message", 1000, messageCallback);
+      ros::Subscriber sub = nh.subscribe("message", 1000, messageCallback);
       ros::NodeHandle n;
       ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
       ros::NodeHandle ne;
       ros::Publisher topic_pub = ne.advertise<geometry_msgs::Point>("topic", 50);
      tf::TransformBroadcaster odom_broadcaster;
-   
+     std::ofstream myfile;
+     myfile.open("fuckz");
    
      ros::Time current_time, last_time;
      current_time = ros::Time::now();
      last_time = ros::Time::now();
-   
+     
      ros::Rate r(1.0);
      while(n.ok())
 {     
@@ -44,14 +59,14 @@ ros::Subscriber sub = nh.subscribe("message", 1000, messageCallback);
         Dr=Distancer-LastDistancer;
         LastDistancel=Distancel;
         LastDistancer=Distancer;
-
+	
         double Dc=0;
 	Dc=(Dl+Dr)/2; 
       current_time = ros::Time::now();
    
        //compute odometry in a typical way given the velocities of the root
-       	double delta_y=Dc*cos(th);
-	double delta_x=Dc*sin(th);
+       	double delta_y=Dc*sin(th);
+	double delta_x=Dc*cos(th);
 	double delta_th=(Dr-Dl)/L; 
        x += delta_x;
        y += delta_y;
@@ -59,16 +74,17 @@ ros::Subscriber sub = nh.subscribe("message", 1000, messageCallback);
    	vx=delta_x/dt;
 	vy=delta_y/dt;
 	vth=delta_th/dt;
-   dth=-th;
+   dth = th;
        //since all odometry is 6DOF we'll need a quaternion created from yaw
 geometry_msgs::Point a;
 a.x=x;
 a.y=y;
 a.z=th;
 topic_pub.publish(a);
-ROS_INFO("y: [%f]", y);
 ROS_INFO("x: [%f]", x);
-       geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(dth);
+ROS_INFO("y: [%f]", y);
+myfile<<"x : "<< x << "  "<<"y : "<<y<<"\n";
+geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(dth);
    
        //first, we'll publish the transform over tf
       geometry_msgs::TransformStamped odom_trans;
@@ -107,5 +123,6 @@ ROS_INFO("x: [%f]", x);
        last_time = current_time;
        r.sleep();
      }
-      
+    myfile.close();       
+  
    }
